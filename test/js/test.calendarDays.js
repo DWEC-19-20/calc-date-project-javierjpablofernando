f5bb61821de36ebd8909b789d353b588e1cdc3a0
Fernando Serrano <fernandohidalgo1994@gmail.com>
2019-10-14 10:16:16 +0200

    asdf

--- test/js/test.calendarDays.js
+++ test/js/test.calendarDays.js
@@ -6,7 +6,7 @@ suite("Testing calcDate", function() {
         expect(calcDate(new Date(2019, 10, 14),14)).to.equal("28/10/2019");
     });
     test("Test pasado", function() {
-        expect(calcDate(new Date(2019, 28, 14),-14)).to.equal("14/10/2019");
+        expect(calcDate(new Date(2019, 14, 28),-14)).to.equal("14/10/2019");
     });
     test("Test pasado", function() {
         expect(calcDate(new Date(2019, 13, 10),10950)).to.equal("5/10/2049");
@@ -18,7 +18,7 @@ suite("Testing getDays", function() {
         expect(getDays(new Date(2019, 10, 14),new Date(2019, 10, 28))).to.equal(14);
     });
     test("Test pasado", function() {
-        expect(calcDate(new Date(2019, 28, 14),new Date(2019, 10, 14)).to.equal(-14);
+        expect(calcDate(new Date(2019, 14, 28),new Date(2019, 10, 14))).to.equal(-14);
     });
     test("Test pasado", function() {
         expect(calcDate(new Date(2019, 13, 10),new Date(2049, 5, 10))).to.equal(10950);
